83f70ab26b4209e8ad294414c35a6a3960e4c653
k07-repo <kpuli007@fiu.edu>
2020-09-26 17:24:33 -0400

    rudimentary front-end mockup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [searchInput, setSearchInput] = useState("")
+  const [results, setResults] = useState("")
+
+  const handleInputChange = (event) => {
+    setSearchInput(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    //prevent entire page from reloading
+    event.preventDefault()
+
+    //try and do search here using search input
+    setResults("You searched for " + searchInput)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSearch}>
+        <input value={searchInput} placeholder="Search term" onChange={handleInputChange}/>
+        <button type="submit">Search</button>
+      </form>
+      <textarea readOnly={true} value={results}/>
     </div>
-  );
+  )
 }
 
 export default App;
